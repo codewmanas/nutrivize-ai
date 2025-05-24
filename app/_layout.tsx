e28603a50506c92f58628f3e55c7739787ec999a
codewmanas <manas.kolaskar@somaiya.edu>
2025-05-24 12:01:40 +0530

    Convex  initialized

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,17 @@
 import { Stack } from "expo-router";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+
 
 export default function RootLayout() {
+  const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+    unsavedChangesWarning: false,
+  });
+
   return (
+    <ConvexProvider client={convex}>
     <Stack screenOptions={{ headerShown: false }} >
       <Stack.Screen name="index" />
     </Stack>
+    </ConvexProvider>
   );
 }
